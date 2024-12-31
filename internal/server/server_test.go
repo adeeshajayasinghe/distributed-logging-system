84f2b45b19ff46c2eeec38e92645bbdacabe1d45
adeeshajayasinghe <jayasingheai.20@uom.lk>
2025-01-01 01:58:21 +0530

    Tested client side TLS

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -47,6 +47,8 @@ func setupTest(t *testing.T, fn func(*Config)) (
 	require.NoError(t, err)
 
 	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile: config.ClientCertFile,
+		KeyFile: config.ClientKeyFile,
 		CAFile: config.CAFile,
 	})
 	require.NoError(t, err)
@@ -66,6 +68,7 @@ func setupTest(t *testing.T, fn func(*Config)) (
 		KeyFile: config.ServerKeyFile,
 		CAFile: config.CAFile,
 		ServerAddress: l.Addr().String(),
+		Server: true,
 	})
 	require.NoError(t, err)
 
